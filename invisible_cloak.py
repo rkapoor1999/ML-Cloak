6b1520d64decd1d90c41c974d2ac2b21ae5a5bc5
Raghav Kapoor <rkapoor1999@RHEAs-MacBook-Pro.local>
2020-08-23 13:37:04 +0530

    added edge detection and color seperation

--- invisible_cloak.py
+++ invisible_cloak.py
@@ -1,45 +1,56 @@
 import cv2
 import numpy as np
-
 cap = cv2.VideoCapture(0)
 back = cv2.imread('./image.jpg')
 
 while cap.isOpened():
-    # take each frame
-    ret, frame = cap.read()
-
-    if ret:
-        # how do we convert rgb to hsv?
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # cv2.imshow("hsv", hsv)
-        # how to get hsv value?
-        # lower: hue - 10, 100, 100, higher: h+10, 255, 255
-        red = np.uint8([[[0,0,255]]]) # bgr value of red
+    ret, frame = cap.read() #capture each frame
+    
+    if ret == True:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # converting the frame captured to hsv from bgr
+        red = np.uint8([[[0,0,255]]])
         hsv_red = cv2.cvtColor(red, cv2.COLOR_BGR2HSV)
-        # get hsv value of red from bgr
-        # print(hsv_red)
-
-        # threshold the hsv value to get only red colors
-        l_red = np.array([0, 100, 100])
-        u_red = np.array([10, 255, 255])
-
-        mask = cv2.inRange(hsv, l_red, u_red)
-        # cv2.imshow("mask", mask)
-
-        # all things red
-        part1 = cv2.bitwise_and(back, back, mask=mask)
-        # cv2.imshow("part1", part1)
-
+        #print(hsv_red)
+        
+        #threshold hsv values to only get red colors alongwith two ranges to provide better color detection
+        #Range for lower range
+        l_red = np.array([0,120, 70])
+        h_red = np.array([10,255,255])
+        mask1 = cv2.inRange(hsv, l_red, h_red)
+        
+        # Range for upper range
+        lower_red = np.array([170,120,70])
+        upper_red = np.array([180,255,255])
+        mask2 = cv2.inRange(hsv,lower_red,upper_red)
+        
+        #using 0-10 and 170-180 to avoid skin inclusion in the range
+        #saturation is taken in between 120-255 assuming the cloth color is highly saturated
+        #value is taken in between 70 to 255 to incorporate red color in the wrinkles of the cloth as well
+        
+        #taking the pixelwise 'OR' of the two masks
+        mask = mask1 +mask2
+        
+        #morphological transformation to refine the mask
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))
+        
+        #all things red
+        part1 = cv2.bitwise_and(back, back, mask = mask)
+        
+        #inverting mask
         mask = cv2.bitwise_not(mask)
-
-        # part 2 is all things not red
-        part2 = cv2.bitwise_and(frame, frame, mask=mask)
-        # cv2.imshow("mask", part2)
-
-        cv2.imshow("cloak", part1 + part2)
-
-        if cv2.waitKey(5) == ord('q'):
+        
+        #all things not red
+        part2 = cv2.bitwise_and(frame, frame, mask = mask)
+        
+        
+        img = part1 + part2
+        
+        cv2.imshow("cloak", img)
+        
+        if(cv2.waitKey(5) == ord('q')):
             break
 
 cap.release()
 cv2.destroyAllWindows()
+        
